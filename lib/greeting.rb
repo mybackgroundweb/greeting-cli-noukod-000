6584f9a70386cc2cfe08f266b44c871b042795ba
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-04 15:31:44 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,8 @@
 # code the #greeting method here!
+puts "Hi! I'm HAL, what's your name?"
+name = "./bin/greet", "name"
+def greeting(name)
+  puts "Hello #{name.strip.chomp}. It's nice to meet you."
+end
+
+greeting("Don")
