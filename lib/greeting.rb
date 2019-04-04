8b1b73649ff77f2a65325cb68d9f1e12ec1a9eaf
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-04 15:26:52 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,7 +2,7 @@
 puts "Hi! I'm HAL, what's your name?"
 name = ["./bin/greet", "name"]
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."
+  puts "Hello #{name.strip.chomp}. It's nice to meet you."
 end
 
 greeting("Don")
